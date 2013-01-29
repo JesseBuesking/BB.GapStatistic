634cd02c56947a7c65b4c2115c49d23ff881560e
Jesse Buesking <jessebuesking@gmail.com>
2013-01-28 23:57:30 -0800

    minor graph improvements

--- __init__.py
+++ __init__.py
@@ -1,13 +1,14 @@
-from matplotlib.collections import PatchCollection
+#from matplotlib.collections import PatchCollection
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 
+
 def gap_statistic(xy, k, iterations):
-    xmin = np.min(xy[:,0])
-    xmax = np.max(xy[:,0])
-    ymin = np.min(xy[:,1])
-    ymax = np.max(xy[:,1])
+    xmin = np.min(xy[:, 0])
+    xmax = np.max(xy[:, 0])
+    ymin = np.min(xy[:, 1])
+    ymax = np.max(xy[:, 1])
 
     ind_runs = 10
     max_iter = 500
@@ -18,10 +19,10 @@ def gap_statistic(xy, k, iterations):
     mean = 0
     for i in range(iterations):
         unif = np.random.uniform(0, 100, (xy.size / 2, 2))
-        unif[:,0] = ((xmax - xmin) * (unif[:,0] - 0)) / (100 - 0)
-        unif[:,1] = ((ymax - ymin) * (unif[:,1] - 0)) / (100 - 0)
-    #        plt.plot(unif[:,0], unif[:,1], 'o')
-    #        plt.show()
+        unif[:, 0] = ((xmax - xmin) * (unif[:, 0] - 0)) / (100 - 0)
+        unif[:, 1] = ((ymax - ymin) * (unif[:, 1] - 0)) / (100 - 0)
+        #        plt.plot(unif[:,0], unif[:,1], 'o')
+        #        plt.show()
         gap_estimator.fit(unif)
         mean += gap_estimator.inertia_
 
@@ -29,9 +30,10 @@ def gap_statistic(xy, k, iterations):
 
     return mean
 
+
 def main():
     k_range = 10
-    values = np.zeros(shape=(k_range,2))
+    values = np.zeros(shape=(k_range, 2))
     np.random.seed(1)
 
     cl1 = np.random.normal(0, 1, (20, 2))
@@ -56,28 +58,37 @@ def main():
         values[k, 0] = estimator.inertia_
         values[k, 1] = mean
 
-    gap_results = np.zeros(shape=(k_range,1))
+    gap_results = np.zeros(shape=(k_range, 1))
     for i in range(1, k_range):
         gap_results[i] = np.log(values[i, 1]) - np.log(values[i, 0])
 
+    max_index = gap_results.argmax(axis=0)
+    fig = plt.figure(1)
+    ax = fig.gca()
+    ax.yaxis.grid(color='0.75', linestyle='dashed')
+    ax.xaxis.grid(color='0.75', linestyle='dashed')
+    ax.text(max_index, gap_results[max_index], "max at k = {}".format(
+        max_index[0]))
     plt.plot([i for i in range(k_range)], gap_results)
-#    plt.plot(xy[:,0], xy[:,1], 'o')
+    plt.scatter(max_index, gap_results[max_index],
+                facecolors='none', edgecolors='r', s=200)
+    #    plt.plot(xy[:,0], xy[:,1], 'o')
 
-#    color_choices = [[round(np.random.uniform(0, 1),1),
-#                      round(np.random.uniform(0, 1),1),
-#                      round(np.random.uniform(0, 1),1)] for _ in range(3)]
-#
-#    colors = ([color_choices[i] for i in estimator.labels_])
-#
-#    plt.scatter(xy[:, 0], xy[:, 1], c=colors)
-#
-#    plt.scatter(estimator.cluster_centers_[:,0],
-#                estimator.cluster_centers_[:,1],
-#                marker='o', s = 200, linewidths=2, c='none')
-#
-#    plt.scatter(estimator.cluster_centers_[:,0],
-#                estimator.cluster_centers_[:,1],
-#                marker='o', s = 150, linewidths=2, c='red')
+    #    color_choices = [[round(np.random.uniform(0, 1),1),
+    #                      round(np.random.uniform(0, 1),1),
+    #                      round(np.random.uniform(0, 1),1)] for _ in range(3)]
+    #
+    #    colors = ([color_choices[i] for i in estimator.labels_])
+    #
+    #    plt.scatter(xy[:, 0], xy[:, 1], c=colors)
+    #
+    #    plt.scatter(estimator.cluster_centers_[:,0],
+    #                estimator.cluster_centers_[:,1],
+    #                marker='o', s = 200, linewidths=2, c='none')
+    #
+    #    plt.scatter(estimator.cluster_centers_[:,0],
+    #                estimator.cluster_centers_[:,1],
+    #                marker='o', s = 150, linewidths=2, c='red')
 
     plt.show()
 
